ae9b20547bd6d58a4606b3c62d6223ea82463be6
phumkrit <su.phumkrit_st@tni.ac.th>
2023-01-15 04:21:58 +0700

    test2

--- react-tailwind/src/Page/Blog.js
+++ react-tailwind/src/Page/Blog.js
@@ -23,7 +23,7 @@ class Blog extends Component {
       <div className="container w-full my-auto h-auto justify-center flex gap-10">
         <div className="rounded-lg h-auto w-64 text-center hover:scale-150 hover:transition mt-10 border-2 border-emerald-600">
           <img src={Dog} class="rounded-lg "></img>
-          <span>Roit is husky</span>
+          <span>testing</span>
         </div>
         <div className="rounded-lg border-2 h-64 w-64 text-center hover:scale-150 hover:transition mt-10">
           <img src={Dog} class="rounded-lg"></img>
